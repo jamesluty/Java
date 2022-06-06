060136e61683f0f008a5fe20cdd5be2d12e4c192
James Luty <luty0125@gmail.com>
2022-06-06 11:27:37 -0700

    initialize Java folder with first java program

---
+++ first_java_program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args){
+        System.out.println("My name is James Luty");
+        System.out.println("I am 36 years old");
+        System.out.println("My hometown is Covington, WA");
+    }
+}
